25ecfa7e4a6fe52f119b63b9cfd6ef485be744d5
Shazna Salman <shaznasalman10@gmail.com>
2026-03-07 12:04:51 +0530

    Initial Commit

---
+++ ClinicFlow_App/ClinicFlow_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlow_AppApp.swift
+//  ClinicFlow_App
+//
+//  Created by Sanduni on 2026-03-07.
+//
+
+import SwiftUI
+
+@main
+struct ClinicFlow_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClinicFlow_AppTests/ClinicFlow_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlow_AppTests.swift
+//  ClinicFlow_AppTests
+//
+//  Created by Sanduni on 2026-03-07.
+//
+
+import Testing
+@testable import ClinicFlow_App
+
+struct ClinicFlow_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
